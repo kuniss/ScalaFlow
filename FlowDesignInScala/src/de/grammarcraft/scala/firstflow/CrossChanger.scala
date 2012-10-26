251a710cd10a5ad4246f9920e192ad0f4a2f73e7
denis.kuniss <denis.kuniss@debln099.wincor-nixdorf.com>
2012-10-26 18:13:02 +0200

    First try implementing flow design in Scala; runs as expected

---
+++ FlowDesignInScala/src/de/grammarcraft/scala/firstflow/CrossChanger.scala
@@ -0,0 +1,57 @@
+package de.grammarcraft.scala.firstflow
+
+import scala.actors.Actor
+
+class CrossChanger(val name: String) extends Actor {
+  
+  case class Input1(msg: Any)
+  case class Input2(msg: Any)
+  
+  var output1: Any => Unit = null
+  var output2: Any => Unit = null
+  
+  def act() {
+    loop {
+      react {
+        case Input1(msg) => {
+        	println(msg + " received as input1 for " + this)
+        	processInput1(msg)
+        }
+        case Input2(msg) => {
+        	println(msg + " received as input2 for " + this)
+        	processInput2(msg)
+        }
+        case msg => println("Unknown msg received: " + msg)
+      }
+    }
+  }
+  
+  def processInput1(msg: Any) {
+    println("process " + msg + " as input1 for " + this)
+    if (output2 == null) {
+    	println("no binding defined for output2")
+    	return
+    }
+    output2(msg)
+  }
+
+  def processInput2(msg: Any) {
+    println("process " + msg + " as input2 for " + this)
+    if (output1 == null) {
+    	println("no binding defined for output2")
+    	return
+    }
+    output1(msg)
+  }
+
+  def input1(msg: Any) {
+    this!Input1(msg)
+  }
+
+  def input2(msg: Any) {
+    this!Input2(msg)
+  }
+  
+  override def toString:String = name
+
+}
---
+++ FlowDesignInScala/src/de/grammarcraft/scala/firstflow/Flow.scala
@@ -0,0 +1,27 @@
+package de.grammarcraft.scala.firstflow
+
+object Flow {
+  def main(args: Array[String]) {
+	  // instantiate
+	  println("instantiate flow units...")
+	  val op1 = new CrossChanger("crossing1")
+	  val op2 = new CrossChanger("crossing2")
+	  
+	  // bind
+	  println("bind them...")
+	  op1.output1 = op2.input1
+	  op1.output2 = op2.input2
+	  op2.output1 = msg => println("received " + msg + " over output1 of " + op2)
+	  op2.output2 = msg => println("received " + msg + " over output2 of " + op2)
+	  
+	  // start
+	  println("run them...")
+	  op1.start()
+	  op2.start()
+	  op1.input1("first message")
+	  op1.input2("second message")
+
+	  println("finished.")
+  }
+
+}
