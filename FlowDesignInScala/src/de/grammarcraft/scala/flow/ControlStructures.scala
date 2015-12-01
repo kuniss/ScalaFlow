46695c0aec7f79e033fcf61b507a0a0d323ec940
Denis Kuniß <kuniss@grammarcraft.de>
2015-12-01 23:46:48 +0100

    documentation improvements, using mark down syntax

--- FlowDesignInScala/src/de/grammarcraft/scala/flow/ControlStructures.scala
+++ FlowDesignInScala/src/de/grammarcraft/scala/flow/ControlStructures.scala
@@ -23,22 +23,24 @@ object ControlStructures {
    * Flow DSl control structure allowing to specify in a code block what should be done with 
    * output data of the given function unit's output port. 
    * Typically used to implement side effects like logging or displaying results.<br>
-   * E.g., <code>
-   * on(<i>fu.output</i>) { 
+   * E.g., 
+   * {{{
+   * on(fu.output) { 
    *  	msg => println("result: " + msg)
    * }
-   * </code>
+   * }}}
    */
   def on[T](outputPort: dsl.OutputPort[T])(operation: T => Unit) { outputPort -> operation }
   
   /**
    * Flow DSl control structure allowing to specify in a code block what should be done for
-   * a set of given function units on an intergation error.<br>
-   * E.g., <code>
-   * onIntegrationError(<i>fu1, fu2, fu3</i>) { 
+   * a set of given function units on an integration error.<br>
+   * E.g., 
+   * {{{
+   * onIntegrationError(fu1, fu2, fu3) { 
    *  	msg => println("integration error happened: " + msg)
    * }
-   * </code>
+   * }}}
    */
   def onIntegrationErrorAt (functionUnitList: FunctionUnit*) (integrationErrorOperation: String => Unit) {
 	  functionUnitList.foreach( functionUnit => functionUnit.onIntegrationError(integrationErrorOperation) )
@@ -47,11 +49,12 @@ object ControlStructures {
   /**
    * Flow DSl control structure allowing to specify in a code block what should be done for
    * a set of given error ports if an error message is arriving through on of these error ports.<br>
-   * E.g., <code>
+   * E.g., 
+   * {{{
    * onError(<i>fu1, fu2, fu3</i>) { 
    *  	msg => println("error happened: " + msg)
    * }
-   * </code>
+   * }}}
    */
   def onErrorAt[ErrorType] (functionUnitWithErrorPortList: ErrorPort[ErrorType]*) (errorOperation: ErrorType => Unit) {
 	  functionUnitWithErrorPortList.foreach { functionUnit => 
